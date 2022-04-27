aaff05a429ede110447fc6709cee99a9587c05f3
Connor Slade <connor@connorcode.com>
2022-04-27 18:44:16 -0400

    🎟 Init Project

---
+++ src/main/java/com/connorcode/universaltick/UniversalTick.java
@@ -0,0 +1,10 @@
+package com.connorcode.universaltick;
+
+import net.fabricmc.api.ModInitializer;
+
+public class UniversalTick implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
