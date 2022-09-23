6fcd1e8db569cb6e819cb6e9c5d73e687cc1495e
Connor Slade <connor@connorcode.com>
2022-09-22 22:58:42 -0400

    Use the sounds pitch when modifying it Also bump version to 1.1.3

--- src/main/java/com/connorcode/universaltick/mixin/ClientSoundSystem.java
+++ src/main/java/com/connorcode/universaltick/mixin/ClientSoundSystem.java
@@ -3,6 +3,7 @@ package com.connorcode.universaltick.mixin;
 import com.connorcode.universaltick.client.UniversalTickClient;
 import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.client.sound.SoundSystem;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,7 +15,8 @@ public class ClientSoundSystem {
     @Inject(method = {"getAdjustedPitch"}, at = {@At("HEAD")}, cancellable = true)
     void modifyPitch(SoundInstance instance, CallbackInfoReturnable<Float> returnable) {
         if (!UniversalTickClient.settings.clientSound) return;
-        returnable.setReturnValue(50.0F / UniversalTickClient.clientTickSpeed);
+        float normalPitch = MathHelper.clamp(instance.getPitch(), 0.5F, 2.0F);
+        returnable.setReturnValue(50.0F / UniversalTickClient.clientTickSpeed * normalPitch);
         returnable.cancel();
     }
 }
